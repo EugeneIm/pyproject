9b0e0a2be8d0e14602d90996c0c803f101eb87df
Your Name <you@example.com>
2021-03-11 14:55:02 -0800

    commit

---
+++ RPS.py
@@ -0,0 +1,37 @@
+import random
+
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
+
+
+#LEARN HOW THIS WORKS PROPERLY
+#IF AND ELIF IS THE SAME AS JS IN THAT PICTURE BORDER LAB
+#understand the "while True:" section 
+#user_action, possible_actions, and computer_action is pretty self explanatory
+        #But what does the "print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")" mean? 
+            #more specifically the  (f"\nYou chose ...  \n")
