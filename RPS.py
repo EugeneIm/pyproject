6bd6132d529c4a8d06b4e43ee90de3d002fe0311
Your Name <you@example.com>
2021-03-11 16:13:55 -0800

    commit

--- RPS.py
+++ RPS.py
@@ -1,6 +1,6 @@
 import random
 
-while True:
+while True:   #WHILE TRUE MEANS THAT IT LOOPS FOREVER/INDEFINITELY BUT SINCE THERE'S THE BREAK AT THE END, THE BREAK CUTS IT OFF 
     user_action = input("Enter a choice (rock, paper, scissors): ")
     possible_actions = ["rock", "paper", "scissors"]
     computer_action = random.choice(possible_actions)
@@ -31,10 +31,16 @@ while True:
 
 #LEARN HOW THIS WORKS PROPERLY
 
+#elif is just a contracted "else if" statement. 
+#I.e., the first section saying that if the values are equal, print "it's a tie"
+#And then the if/else statement under it, followed by another elif for the case of input being paper, scissors, etc. 
+
+
 #if user_action == computer_action: 
     #print(f"both players selected {user_action}. It's a tie!")
                 #^^ The above is the syntax for a tie in the RPS
-                
+
+
 #IF AND ELIF IS THE SAME AS JS IN THAT PICTURE BORDER LAB
 #understand the "while True:" section 
 #user_action, possible_actions, and computer_action is pretty self explanatory
