3f594a71a3ebce00cad7f0db6da822f67cf9014e
Your Name <you@example.com>
2021-03-12 22:24:54 -0800

    commit

--- guess.py
+++ guess.py
@@ -1,14 +1,20 @@
 import random
- 
-hidden = random.randrange(1, 201)
- 
-guess = int(input("Please enter your guess: "))
- 
-if guess == hidden:
-    print("Hit!")
-elif guess < hidden:
-    print("Your guess is too low")
-else:
-    print("Your guess is too high")
 
-print(hidden)
+while True:
+    hidden = random.randrange(1, 201)
+ 
+    guess = int(input("Please enter your guess: "))
+ 
+    if guess == hidden:
+        print("Hit!")
+    elif guess < hidden:
+        print("Your guess is too low")
+    else:
+        print("Your guess is too high")
+
+    print(hidden)
+    
+    play_again = input("Try again? (y/n): ")
+    if play_again.lower() != "y":
+        print("Thank you for playing")
+        break
