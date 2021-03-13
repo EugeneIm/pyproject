047994a6af43c774f5268c102eb3e6d30d731720
Your Name <you@example.com>
2021-03-12 22:15:35 -0800

    commit

--- RockPaperSc.py
+++ RockPaperSc.py
@@ -10,19 +10,19 @@ while True:   #WHILE TRUE MEANS THAT IT LOOPS FOREVER/INDEFINITELY BUT SINCE THE
         print(f"Both players selected {user_action}. It's a tie!")
     elif user_action == "rock":
         if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+            print("Rock > scissors! You win!")
         else:
-            print("Paper covers rock! You lose.")
+            print("Paper > rock! You lose.")
     elif user_action == "paper":
         if computer_action == "rock":
-            print("Paper covers rock! You win!")
+            print("Paper > rock! You win!")
         else:
-            print("Scissors cuts paper! You lose.")
+            print("Scissors > paper! You lose.")
     elif user_action == "scissors":
         if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            print("Scissors > paper! You win!")
         else:
-            print("Rock smashes scissors! You lose.")
+            print("Rock > scissors! You lose.")
 
     play_again = input("Play again? (y/n): ")
     if play_again.lower() != "y":
