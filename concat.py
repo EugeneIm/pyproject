5e76a0edd2483a8f96dcbf7aef1d627a2a20e865
EugeneIm <immer277@gmail.com>
2021-03-15 20:57:50 -0700

    commit

---
+++ concat.py
@@ -0,0 +1,17 @@
+# concatenating a string is just adding two strings together
+# i.e.
+
+x = 10
+y = 20
+
+z = x + y 
+print(z)
+# the end print is "30" because Z = x + y which equals to z = 10 + 20.
+
+"""
+x = 5
+y = jacob
+
+print(x + y)
+"""
+# the syntax above will give you an error because it can't combine a string and a number.
